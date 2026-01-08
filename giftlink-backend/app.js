56b28ddd040444472251b1f0c5b8e8ed0dcb4018
Tawanda Nduna Mashoko <mashokotawanda61@gmail.com>
2026-01-08 15:06:51 +0100

    Updated search and gift Routes

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -9,7 +9,7 @@ const {loadData} = require("./util/import-mongo/index");
 
 
 const app = express();
-app.use("/",cors());
+app.use("*",cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
@@ -22,25 +22,16 @@ connectToDatabase().then(() => {
 app.use(express.json());
 
 // Route files
-// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
-const giftRoutes = require('./routes/giftRoutes')
-
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-const searchRoutes = require('./routes/searchRoutes')
-
-
+const giftRoutes = require('./routes/giftRoutes');
+const searchRoutes = require('./routes/searchRoutes');
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
 // Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
 app.use('/api/gifts', giftRoutes);
-
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
-
 
 // Global Error Handler
 app.use((err, req, res, next) => {
