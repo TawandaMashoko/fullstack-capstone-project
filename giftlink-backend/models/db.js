17733ddeca6c95b77a960ffc33094f2425eaedd6
Tawanda Nduna Mashoko <mashokotawanda61@gmail.com>
2026-01-12 23:53:50 +0100

    Backend API's for frontend components

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -1,35 +1,32 @@
 /* jshint esversion: 8 */
 
-// db.js
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
 // MongoDB connection URL with authentication options
-let url = `${process.env.MONGO_URL}`;
-
+const url = `${process.env.MONGO_URL}`;
 let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
+    if (dbInstance) {
+        return dbInstance;
+    }
 
-    // create a new mongodb client instance
-    const client = new MongoClient(url);      
+    // Create a new MongoDB client instance
+    const client = new MongoClient(url);
 
     try {
-        // Task 1 Connect to DB
+        // Task 1: Connect to DB
         await client.connect();
 
-        //Task 2: Connect to database giftDB and store in variable 
+        // Task 2: Connect to database giftdb and store in variable
         dbInstance = client.db(dbName);
 
         console.log("Connected to MongoDB:", dbName);
 
         // Task 3: Return the DB Instance
         return dbInstance;
-
     } catch (error) {
         console.log("Error connecting to MongoDB:", error);
         throw error;
